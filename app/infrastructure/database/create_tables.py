5ab2a54007316ce87809c490d5ba16a154b884cd
ljh1114 <wnsgud1114@gmail.com>
2026-05-01 09:49:23 +0900

    feat : [이준형] Backend 서버가 추천결과 카드 클릭 이벤트를 저장한다 [MJE-BE-9]

--- app/infrastructure/database/create_tables.py
+++ app/infrastructure/database/create_tables.py
@@ -2,6 +2,7 @@ import app.domains.home.repository.orm.home_event_orm  # noqa: F401
 import app.domains.recommendation.repository.orm.recommendation_request_orm  # noqa: F401
 import app.domains.courses.repository.orm.course_orm  # noqa: F401
 import app.domains.courses.repository.orm.course_place_orm  # noqa: F401
+import app.domains.courses.repository.orm.courses_event_orm  # noqa: F401
 import app.infrastructure.api.repository.orm.email_log_orm  # noqa: F401
 
 from app.infrastructure.database.base import Base
